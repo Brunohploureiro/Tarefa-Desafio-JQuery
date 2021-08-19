7f322c639dc9500afd885e2d3e43fd3f3a139d81
Bruno Henrique Henrique <85626791+Brunohploureiro@users.noreply.github.com>
2021-08-19 14:39:05 +0000

    3

--- js/jquery-perzonalizador.js
+++ js/jquery-perzonalizador.js
@@ -1,5 +1,29 @@
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
+$(document).ready(function(){
+  $('a').click(function(e){
+    e.preventDefault()
+
+   let pagina = $(this).attr('href')
+
+   $('.modal-title').empty()
+   $('.modal-body').empty()
+
+   switch(pagina){
+     case'Lamborghini Urus':
+      $('.modal-title').append('Lamborghini Urus') 
+      $('.modal-body').append('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, nostrum ex placeat minima quisquam illum rem doloribus tenetur, enim, quidem iure deleniti sint laborum nulla excepturi dignissimos maxime id numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>')
+      break
+     case'Ford Mustang': 
+      $('.modal-title').append('Ford Mustang')
+      $('.modal-body').append('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, nostrum ex placeat minima quisquam illum rem doloribus tenetur, enim, quidem iure deleniti sint laborum nulla excepturi dignissimos maxime id numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>')
+      break
+     case'Chevrolet Camaro': 
+      $('.modal-title').append('Chevrolet Camaro') 
+      $('.modal-body').append('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, nostrum ex placeat minima quisquam illum rem doloribus tenetur, enim, quidem iure deleniti sint laborum nulla excepturi dignissimos maxime id numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>')
+      break
+     default: alert('Pagina não encontrada')
+   }
+
+    $('#modal-info').modal('show')
+
+  })
+})
